2589a5eceee7317594161fa4ed823d1f99199f81
guenga1968 <gustavogallesio@hotmail.com>
2022-03-27 17:39:31 -0300

    base y routes 2

--- api/src/models/Tourism.js
+++ api/src/models/Tourism.js
@@ -40,7 +40,7 @@ module.exports = (sequelize) => {
         },
         description: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: true,
         },
     });
 };
